e262807c70dddaabe5376cef32b6fa21313ce270
ashishpanwar0 <ashishpanwar5684@gmail.com>
2025-04-24 12:54:25 +0530

    Sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,45 +2,44 @@ import React from 'react'
 import { LogOut, UserCircle2 } from 'lucide-react';
 
 function Sidebar() {
-  return (
-    <div className="bg-[#0b1f4e] text-white w-64 min-h-screen flex flex-col justify-between">
-    <div>
-      {/* Top Profile Section */}
-      <div className="flex items-center gap-2 px-4 py-6 border-b border-white/10">
-        <UserCircle2 size={32} />
-        <div className="text-sm leading-tight">
-          <p className="font-semibold">Shree Kaushal Prem</p>
-          <p className="text-xs text-white/70">Metal</p>
+    return (
+        <div className="bg-[#0b1f4e] text-white w-64 min-h-screen flex flex-col justify-between">
+            <div>
+                {/* Top Profile Section */}
+                <div className="flex items-center gap-2 px-4 py-6 border-b border-white/10">
+                    <UserCircle2 size={32} />
+                    <div className="text-sm leading-tight">
+                        <p className="font-semibold">Shree Kaushal Prem</p>
+                        <p className="text-xs text-white/70">Metal</p>
+                    </div>
+                </div>
+
+                <nav className="mt-4 space-y-2 px-4">
+                    <a href=""><NavItem label="Dashboard" /></a>
+                    <a href=""><NavItem label="customer" /></a>
+                    <a href=""><NavItem label="inventory" /></a>
+                    <a href=""><NavItem label="Settings" /></a>
+                </nav>
+            </div>
+
+
+            <div className="px-4 py-6">
+                <button className="flex items-center gap-2 text-white hover:text-red-500">
+                    <LogOut size={18} />
+                    <span>LOGOUT</span>
+                </button>
+            </div>
         </div>
-      </div>
-
-      {/* Navigation Links */}
-      <nav className="mt-4 space-y-2 px-4">
-        <NavItem label="Dashboard" />
-        <NavItem label="customer" />
-        <NavItem label="inventory" />
-        <NavItem label="Settings" />
-      </nav>
-    </div>
-
-    {/* Logout Section */}
-    <div className="px-4 py-6">
-      <button className="flex items-center gap-2 text-white hover:text-red-500">
-        <LogOut size={18} />
-        <span>LOGOUT</span>
-      </button>
-    </div>
-  </div>
-);
+    );
 }
 
 function NavItem({ label }) {
-return (
-  <div className="flex items-center gap-2 px-2 py-2 hover:bg-white/10 rounded-lg cursor-pointer">
-    <div className="h-3 w-3 bg-gray-300 rounded-full" />
-    <span className="capitalize">{label}</span>
-  </div>
-  )
+    return (
+        <div className="flex items-center gap-2 px-2 py-2 hover:bg-white/10 rounded-lg cursor-pointer">
+            <div className="h-3 w-3 bg-gray-300 rounded-full" />
+            <span className="capitalize">{label}</span>
+        </div>
+    )
 }
 
 export default Sidebar
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,7 +3,16 @@ import Sidebar from '../Components/Sidebar'
 const Dashboard = () => {
   return (
     <>
+    <section className='flex'>
+      <div>
     <Sidebar/>
+    </div>
+    <div className='bg-[#0b1f4e] h-10 w-full text-white flex gap-15 text-sm'>
+      <p className='ml-90 mt-2'>Welcome Back</p>
+      <p className='mt-2'>21 April 2025</p>
+      <p className='mt-2'>Monday</p>
+    </div>
+    </section>
     </>
   )
 }
